0037d91b4e4007a2656f16469c3209169156c34c
Dania <27072003danya@gmail.com>
2022-12-23 20:05:22 +0200

    task-1 and task-2

---
+++ ДЗ_1/task-1.js
@@ -0,0 +1,6 @@
+let name = 'Генератор захисного поля', price = 1000;
+let resName1 = `Обрано ${name}, ціна за штуку ${price} кредитів`;
+console.log(resName1);
+price = 2000;
+let resName2 = `Обрано ${name}, ціна за штуку ${price} кредитів`;
+console.log(resName2);
