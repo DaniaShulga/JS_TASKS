8a30a57cd7ccabd624fb751e342eaa010236f704
Dania <27072003danya@gmail.com>
2022-12-28 14:45:43 +0200

    task-6 changed

--- HW_1/task-6.js
+++ HW_1/task-6.js
@@ -1,20 +1,19 @@
 let input = prompt('Введіть число:');
 let total = 0;
-let i = 0;
 
     if(isNaN(input)){
         input = 0;
         alert('Було написано не число, спробуйте ще раз')
+        input = prompt('Введіть число:');
     }
 
-    while (input !== null) {
-        i += 1;
-        total += Number(input);
-        input = prompt('Введіть ще число:');
+    for(let i = 0; i < input; i++){
+        if (input !== null) {
+            total += Number(input);
+            input = prompt('Введіть ще число:');
+        }
     }
-
-   alert(`Загальна сума чисел дорівнює ${total}`)
-
+    alert(`Загальна сума чисел дорівнює ${total}`)
 
 
 
